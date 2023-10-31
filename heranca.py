55526b4eb029b670aca70504aa22ee1e53d80f00
Mariana Laura Franco Raymundo <marianalaurafrancoraymundo@gmail.com>
2023-10-31 19:42:07 -0300

    cap08-heranca

---
+++ heranca.py
@@ -0,0 +1,48 @@
+# Criando a classe Animal - Super-classe
+class Animal:
+    
+    def __init__(self):
+        print("Animal criado.")
+
+    def imprimir(self):
+        print("Este é um animal.")
+
+    def comer(self):
+        print("Hora de comer.")
+        
+    def emitir_som(self):
+        pass
+
+# Criando a classe Cachorro - Sub-classe
+class Cachorro(Animal):
+    
+    def __init__(self):
+        Animal.__init__(self)
+        print("Objeto Cachorro criado.")
+    
+    def emitir_som(self):
+        print("Au au!")
+
+# Criando a classe Gato - Sub-classe
+class Gato(Animal):
+    
+    def __init__(self):
+        Animal.__init__(self)
+        print("Objeto Gato criado.")
+    
+    def emitir_som(self):
+        print("Miau!")
+
+# Criando um objeto (Instanciando a classe)
+rex = Cachorro()
+
+# Criando um objeto (Instanciando a classe)
+zeze = Gato()
+
+rex.emitir_som() 
+
+zeze.emitir_som()
+
+rex.imprimir()
+
+zeze.comer()
